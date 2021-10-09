11555d276fb30de6591d75bae1541400fffcd500
SavitskayaKseniya22 <77901301+SavitskayaKseniya22@users.noreply.github.com>
2021-10-10 01:48:05 +0300

    feat: fix forms

--- museum-dom/js/tickets.js
+++ museum-dom/js/tickets.js
@@ -2,9 +2,8 @@ let minuses = document.querySelectorAll(".minus")
 let pluses = document.querySelectorAll(".plus")
 let totalSum = document.querySelector(".total-sum")
 let ticketType = document.querySelector(".ticket-type")
-
-let basicValue = document.querySelector(".amount .basic input[type='number']")
-let seniorValue = document.querySelector(".amount .senior input[type='number']")
+let basicValue = document.querySelector(" .basic input[type='number']")
+let seniorValue = document.querySelector(" .senior input[type='number']")
 
 
 myStorage = window.localStorage;
@@ -17,7 +16,13 @@ window.onload = function () {
     let inputs = document.querySelectorAll(".counter input[type='number']")
     inputs[0].value = myStorage.basicValue
     inputs[1].value = myStorage.seniorValue
-    totalSum.innerText = myStorage.totalSum
+    if (myStorage.totalSum) {
+        totalSum.innerText = myStorage.totalSum
+    } else {
+        myStorage.totalSum = totalSum.innerText;
+    }
+
+
 
 }
 for (const item of pluses) {
@@ -25,13 +30,16 @@ for (const item of pluses) {
         let input = (item.parentNode).querySelector("input")
         input.value++;
         if (item.parentNode.className == "counter basic") {
-            totalSum.innerText = Number(totalSum.innerText) + getCost()
+            myStorage.totalSum = Number(totalSum.innerText) + getCost()
             myStorage.basicValue++;
-            myStorage.totalSum = totalSum.innerText;
+            totalSum.innerText = myStorage.totalSum;
+            totalSumOverwiew.innerText = myStorage.totalSum;
         } else {
-            totalSum.innerText = Number(totalSum.innerText) + (getCost() / 2)
+            myStorage.totalSum = Number(totalSum.innerText) + (getCost() / 2)
             myStorage.seniorValue++;
-            myStorage.totalSum = totalSum.innerText;
+            totalSum.innerText = myStorage.totalSum;
+            totalSumOverwiew.innerText = myStorage.totalSum
+
         }
     })
 }
@@ -42,13 +50,17 @@ for (const item of minuses) {
         if (input.value >= 1) {
             input.value--;
             if (item.parentNode.className == "counter basic") {
-                totalSum.innerText = Number(totalSum.innerText) - getCost()
+                myStorage.totalSum = Number(totalSum.innerText) - getCost()
                 myStorage.basicValue--;
-                myStorage.totalSum = totalSum.innerText;
+                totalSum.innerText = myStorage.totalSum;
+                totalSumOverwiew.innerText = myStorage.totalSum;
+
             } else {
-                totalSum.innerText = Number(totalSum.innerText) - (getCost() / 2);
+                myStorage.totalSum = Number(totalSum.innerText) - (getCost() / 2)
                 myStorage.seniorValue--;
-                myStorage.totalSum = totalSum.innerText;
+                totalSum.innerText = myStorage.totalSum
+                totalSumOverwiew.innerText = myStorage.totalSum;
+
             }
         }
     })
@@ -59,13 +71,15 @@ for (const item of ticketTypeInputs) {
     item.onchange = function () {
         recalculation()
         myStorage.ticketTypeAcive = ticketType.querySelector("input[type='radio']:checked").id;
-        myStorage.totalSum = totalSum.innerText;
+
     }
 }
 
 
 function recalculation() {
-    totalSum.innerText = basicValue.value * getCost() + seniorValue.value * (getCost() / 2)
+    myStorage.totalSum = basicValue.value * getCost() + seniorValue.value * (getCost() / 2)
+    totalSum.innerText = myStorage.totalSum
+    totalSumOverwiew.innerText = myStorage.totalSum;
 }
 
 function getCost() {
@@ -87,7 +101,61 @@ function getCost() {
 
 //выезжающая форма
 let ticketsAmountPopup = document.querySelectorAll(".ticketsAmount input[type='number']")
+let totalSumOverwiew = document.querySelector(".total-sum-overwiew")
+let totalBasic = document.querySelector(".total-basic")
+let totalSenior = document.querySelector(".total-senior")
+
+let costBasic = document.querySelectorAll(".basic-cost")
+let costSenior = document.querySelectorAll(".senior-cost")
+
+let numSummarys = document.querySelectorAll(".num-summary")
+
+
 buyButton.addEventListener("click", function () {
     ticketsAmountPopup[0].value = myStorage.basicValue
     ticketsAmountPopup[1].value = myStorage.seniorValue
+
+    totalSumOverwiew.innerText = myStorage.totalSum
+    totalBasic.innerText = myStorage.basicValue * getCost()
+    totalSenior.innerText = myStorage.seniorValue * (getCost() / 2)
+
+    costBasic[0].innerText = getCost()
+    costBasic[1].innerText = getCost()
+    costSenior[0].innerText = getCost() / 2
+    costSenior[1].innerText = getCost() / 2
+
+    numSummarys[0].innerText = myStorage.basicValue
+    numSummarys[1].innerText = myStorage.seniorValue
 })
+
+let buttons = document.querySelectorAll(".ticketsAmount button")
+
+for (const item of buttons) {
+
+    item.addEventListener("click", function () {
+
+        myStorage.basicValue = ticketsAmountPopup[0].value
+        myStorage.seniorValue = ticketsAmountPopup[1].value
+
+        basicValue.value = myStorage.basicValue
+        seniorValue.value = myStorage.seniorValue
+
+        numSummarys[0].innerText = myStorage.basicValue
+        numSummarys[1].innerText = myStorage.seniorValue
+
+        totalBasic.innerText = myStorage.basicValue * getCost()
+        totalSenior.innerText = myStorage.seniorValue * (getCost() / 2)
+
+
+    })
+}
+
+
+
+
+//дата
+var dateControl = document.querySelector('#booking-tickets input[type="date"]');
+let nowDate = new Date();
+let str = "";
+str = nowDate.getFullYear() + "-" + nowDate.getMonth() + "-" + nowDate.getDate()
+alert(str)
