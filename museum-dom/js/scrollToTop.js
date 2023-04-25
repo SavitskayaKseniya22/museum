46b4f42b5879627d1471d342ea7cca7187476f3d
Kseniia Savitskaia <savitskaya.kseniya.22@yandex.ru>
2023-04-25 17:27:29 +0300

    docs: delete files

--- museum-dom/js/scrollToTop.js
+++
@@ -1,8 +0,0 @@
-let arrow = document.querySelector('.arrowScroll');
-arrow.addEventListener('click', function (event) {
-    scrollTo(0, 0)
-});
-
-window.addEventListener('scroll', function () {
-    arrow.hidden = (window.pageYOffset < document.documentElement.clientHeight);
-});
